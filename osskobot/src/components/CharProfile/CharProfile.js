47f724ebd37241703c59e5a24a0f6ef98669da88
rktlskan021 <rktlskan021@gmail.com>
2024-08-24 17:20:07 +0900

    refacor: 코드 리펙터링
    등장인물 소개 모달 삭제

--- osskobot/src/components/CharProfile/CharProfile.js
+++ osskobot/src/components/CharProfile/CharProfile.js
@@ -1,32 +1,17 @@
 import styles from './CharProfile.module.css';
-import Modal from '../Modal/CharIntroModal';
 import styled from 'styled-components';
-import React, { useState } from 'react';
+import React from 'react';
 
 const Image = styled.img`
     width: ${props => props.$size}px;
     border-radius: 500px;
-    cursor: pointer;
 `;
 
-function CharProfile({character, mode, size=100, onClick}){
-    const [isClk, setIsClk] = useState(false);
-
-    const onClickBtn = () => {
-        setIsClk(true);
-    };
-
-    const onRequestClose = () => {
-        setIsClk(false);
-    };
-
+function CharProfile({character, size=100}){
     return (
         <div className={styles.profileDiv}>
-            <button className={styles.profileBtn} onClick={mode===1 ? onClickBtn : onClick} >
-                <Image src={character.character_image} alt={character.name} $size={size} />
-            </button>
+            <Image src={character.character_image} alt={character.name} $size={size} />
             <p className={styles.name}>{character.name}</p>
-            <Modal isOpen={isClk} onRequestClose={onRequestClose} name={character.name} description={character.greeting} src={character.character_image} mode={mode} content={`${character.description}`} characterid={character.id}/>
         </div>
     )
 };
